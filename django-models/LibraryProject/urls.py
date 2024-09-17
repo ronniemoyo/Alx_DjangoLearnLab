d2943c76cd6fda2f1672e23636b9c5963a304ddb
ronniemoyo <moyoronnie1@gmail.com>
2024-09-17 20:26:07 +0200

    Set up URL routing and fix root path
    Add redirect from root URL to books list view
    Verify URL patterns in relationship_app
    Ensure relationship_app is in INSTALLED_APPS
    Troubleshoot 404 errors for main pages

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship/', include('relationship_app.urls')),
+    path('', RedirectView.as_view(url='/relationship/books/', permanent=True)),
 ]
